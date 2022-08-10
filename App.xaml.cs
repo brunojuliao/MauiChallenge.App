658de76f677e8a3da47b3293c0051fb8a9a12aac
Bruno Julião <2057387+brunojuliao@users.noreply.github.com>
2022-08-10 17:27:12 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiChallenge;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiChallenge;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
